2a0dfef904b1c3b360086b4bf5da70027ef040c0
Ivan <irodkin2002@gmail.com>
2021-03-21 15:26:56 +0300

    Unit tests redirected to other file

---
+++ Tests/main_test.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cassert>
+#include "../MainHeader.h"
+int main(){
+    auto* p = new Pyromancer;
+    auto* f = new ImpFactory;
+    Enemy* e = f->createEnemy(3);
+    assert(e->LVL == 3);
+    std::cout << "###########################" << '\n';
+    std::cout << "#   Factory test passed   #" << '\n';
+    std::cout << "###########################" << '\n';
+    Imp* imp = dynamic_cast<Imp*>(e);
+    assert(imp->LVL==e->LVL);
+    std::cout << "###########################" << '\n';
+    std::cout << "# Inheritance test passed #" << '\n';
+    std::cout << "###########################" << '\n';
+
+    delete f;
+    delete imp;
+    delete p;
+}
