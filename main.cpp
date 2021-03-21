c1123a6e64cd78bb45280a868060369dcc1c30e8
Ivan <irodkin2002@gmail.com>
2021-03-21 14:56:35 +0300

    Unit tests formatted

--- main.cpp
+++ main.cpp
@@ -5,9 +5,16 @@ int main() {
     auto* p = new Pyromancer;
     auto* f = new ImpFactory;
     Enemy* e = f->createEnemy(3);
+    assert(e->LVL == 3);
+    std::cout << "###########################" << '\n';
+    std::cout << "#   Factory test passed   #" << '\n';
+    std::cout << "###########################" << '\n';
     Imp* imp = dynamic_cast<Imp*>(e);
     assert(imp->LVL==e->LVL);
-    std::cout << "Inheritance test passed";
+    std::cout << "###########################" << '\n';
+    std::cout << "# Inheritance test passed #" << '\n';
+    std::cout << "###########################" << '\n';
+
     delete f;
     delete imp;
     delete p;
